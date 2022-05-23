f1aa40c33a1bbcd3e7ffb9d0962edff21f79749e
Maria Gomez <maria_gg86@yahoo.es>
2022-05-23 10:38:25 +0200

    POM.xml file and dependencies

---
+++ src/main/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+    @Test
+    public void nativeAPAndroid() {
+
+        System.out.println("nativeAPAndroid");
+    }
+
+    @Test
+    public void IOSApps() {
+
+        System.out.println("IOSApps");
+
+    }
+}
